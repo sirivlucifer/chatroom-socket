f0771516a10a3a791a820d9071d8e76b74d34827
Erdem <eerdemlucifer@gmail.com>
2021-12-19 17:54:52 +0300

    minor

--- client.py
+++ client.py
@@ -5,13 +5,13 @@ nickname = input("Kullanici adinizi yaziniz: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #sock_stream tcp tanımlama
 #AF_INET, IPv4 için İnternet adresi ailesidir. SOCK_STREAM, mesajlarımızı ağda taşımak için kullanılacak protokol olan TCP için soket tipidir.
-client.connect(('127.0.0.1', 99999))
+client.connect(('127.0.0.1', 55555))
 
 def receive():
     while True:
         try:
             message = client.recv(1024).decode('ascii')
-            if message == 'Kullanici Adi:':
+            if message == 'NICK':
                 client.send(nickname.encode('ascii'))
             else:
                 print(message)
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 host = '127.0.0.1' #local
-port = 99999
+port = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
@@ -11,20 +11,18 @@ server.listen()
 clients = []
 nicknames = []
 
-# Sending Messages To All Connected Clients
+
 def broadcast(message):
     for client in clients:
         client.send(message)
 
-        # Handling Messages From Clients
 def handle(client):
     while True:
         try:
-            # Broadcasting Messages
             message = client.recv(1024)
             broadcast(message)
         except:
-            # Removing And Closing Clients
+            # SİLİNEBİLİR
             index = clients.index(client)
             clients.remove(client)
             client.close()
@@ -33,25 +31,24 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
-# Receiving / Listening Function
 def receive():
     while True:
-        # Accept Connection
+       
         client, address = server.accept()
-        print("Beraber katildi {}".format(str(address)))
+        print("Ip ve Port {}".format(str(address)))
 
-        # Request And Store Nickname
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
 
-        # Print And Broadcast Nickname
+    
         print("Kullanici adi: {}".format(nickname))
         broadcast("{} katildi!".format(nickname).encode('ascii'))
+        #print("{} portu ile katildi".port)
         client.send('Sunucuya katildi!'.encode('ascii'))
 
-        # Start Handling Thread For Client
+        
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
